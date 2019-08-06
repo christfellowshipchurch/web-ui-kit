df806624b5b0ec828baf3ed34c741071dd2303e6
Daniel Wood <dzwood327@gmail.com>
2019-08-06 12:53:41 -0400

    Updated Imports

--- src/components/Block/__tests__/BlockContainer.tests.js
+++ src/components/Block/__tests__/BlockContainer.tests.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Block from '../../Block'
+import Block from '..'
 import renderer from 'react-test-renderer'
 
 describe('BlockContainer', () => {
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Loader from './Loader'
 import Media from './Media'
 import Block from './Block'
+import Row from './Row'
+import Tools from './Tools'
 
-export { Loader, Media, Block }
+export { Loader, Media, Block, Row, Tools }
