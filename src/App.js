ce47216ef2fc9d83d09c6eece0abae9e8acbb973
Daniel Wood <dzwood327@gmail.com>
2019-08-06 12:55:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from 'react'
 import './components/styles/css/christ-fellowship-bootstrap.css'
-
+import {Block, Row} from './components'
 
 const App = () => {
+
+  const mediaProps = {
+    layout: 'default',
+    imageUrl: 'https://rock.christfellowshipconference.com/GetImage.ashx?guid=2550f1e3-271d-4156-96ff-cfcac1345731',
+    imageAlt: 'this is an an image',
+    videoUrl: null,
+    ratio: '16by9',
+    className: 'smaller-img',
+    rounded: false,
+    media: null
+  }
 
   return (
     <div className="container">
       <div className="row">
         <div className="col text-center">
-          <h1 className="text-primary">Hello There!</h1>
-          <button className="btn btn-primary">Default button</button>
-          <br></br>
-          <button className="btn btn-lg btn-primary">Large button</button>
-          <br></br>
-          <button className="btn btn-sm btn-primary">Small button</button>
+          <Block className='bg-dark' media={mediaProps}>
+            <Block.Title>
+              Block Title
+            </Block.Title>
+            <Block.Subtitle>
+              Subtitle
+            </Block.Subtitle>
+          </Block>
+
+          <Row>
+            
+          </Row>
+
         </div>
       </div>
     </div>
