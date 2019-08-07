565aea6120544d738cc79e9c78f1195704201f4f
Daniel Wood <dzwood327@gmail.com>
2019-08-07 14:18:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,61 @@
 import React from 'react'
 import './components/styles/css/christ-fellowship-bootstrap.css'
-import {Block, Row} from './components'
+import {Block, Button, Tools, Media, Row} from './components'
+
+const multipleContent =[
+  {
+    backgroundColor: '#353535',
+    contentLayout: 'right',
+    callsToAction: {call: 'learn more', action: '#action'},
+    coverImage: {sources: {0:{uri: 'https://rock.christfellowshipconference.com/GetImage.ashx?guid=2550f1e3-271d-4156-96ff-cfcac1345731'}}},
+    imageRatio: "1by1",
+    htmlContent: 'Discover more about our pastors, beliefs, and history',
+    // subtitle: 'Subtitle',
+    title: 'Want to learn more about Christ Fellowship?'
+  },
+  {
+    backgroundColor: '#ffffff',
+    contentLayout: 'right',
+    // callsToAction: {call: 'learn more', action: '#action'},
+    // coverImage: {sources: {0:{uri: 'https://rock.christfellowshipconference.com/GetImage.ashx?guid=2550f1e3-271d-4156-96ff-cfcac1345731'}}},
+    // imageRatio: "16:9",
+    title: '5 Steps to Building a Healthy Life',
+    // subtitle: 'Subtitle',
+    htmlContent: 'Sign up to receive this free resource and start becoming better you today'
+  },
+  {
+    backgroundColor: '#535353',
+    contentLayout: 'left',
+    callsToAction: {call: 'learn more', action: '#action'},
+    coverImage: {sources: {0:{uri: 'https://rock.christfellowshipconference.com/GetImage.ashx?guid=2550f1e3-271d-4156-96ff-cfcac1345731'}}},
+    imageRatio: "1by1",
+    title: 'Want to learn more about Christ Fellowship?',
+    // subtitle: 'Subtitle',
+    htmlContent: 'Discover more about our pastors, beliefs, and history'
+  }
+]
+const content = {
+  backgroundColor: '#353535',
+  contentLayout: 'default',
+  callsToAction: {call: 'learn more', action: '#action'},
+  coverImage: {sources: {0:{uri: 'https://rock.christfellowshipconference.com/GetImage.ashx?guid=2550f1e3-271d-4156-96ff-cfcac1345731'}}},
+  imageRatio: '16by9',
+  htmlContent: 'Discover more about our pastors, beliefs, and history',
+  subtitle: 'Subtitle',
+  title: 'Want to learn more about Christ Fellowship?'
+}
+
 
 const App = () => {
 
-  const mediaProps = {
-    layout: 'default',
-    imageUrl: 'https://rock.christfellowshipconference.com/GetImage.ashx?guid=2550f1e3-271d-4156-96ff-cfcac1345731',
-    imageAlt: 'this is an an image',
-    videoUrl: null,
-    ratio: '16by9',
-    className: 'smaller-img',
-    rounded: false,
-    media: null
-  }
-
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col text-center">
-          <Block className='bg-dark' media={mediaProps}>
-            <Block.Title>
-              Block Title
-            </Block.Title>
-            <Block.Subtitle>
-              Subtitle
-            </Block.Subtitle>
-          </Block>
 
-          <Row>
-            
-          </Row>
+         <div>
+            <Row content={multipleContent} />
+            <Tools.renderBlock content={content} />
+         </div>
 
-        </div>
-      </div>
-    </div>
-  )
+          )
 }
 
 export default App
