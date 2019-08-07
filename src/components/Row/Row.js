5f2eddd5f081664920e070d450e46999b105d20b
Daniel Wood <dzwood327@gmail.com>
2019-08-07 16:46:48 -0400

    Update Row.js

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,41 +1,52 @@
 import React from 'react'
-import { Tools, } from '../../components'
+import classnames from 'classnames'
+import { renderBlock } from '../utils'
 
 
 const Row = ({ content }) => {
+    const lastBlock = content[content.length - 1]
+    const classes = {
+        row: classnames(
+            'row',
+            'align-items-center'
+        ),
+        col: classnames(
+            'col-12',
+            'col-md-6',
+            'p-0'
+        )
+    }
 
-let lastBlock = content[content.length-1]
-
-if(content.length % 2 == 0) {
-    return (
-        <div className='row'>          
-        {content.map((child, i) => {             
-           return (
-                <div className='col-6 p-0'>                   
-                    <Tools.renderBlock content={child} />
-                </div>
-                )
-        })}                
-        </div>
-        )    
-} else {   
-    return (
-        <div className='row'>
-        {content.map((child, i) => { 
-        content.pop()            
-           return (
-                <div className='col-6 p-0'>                   
-                    <Tools.renderBlock content={child} />
-                </div>
-                )
-        })}
-            <div className='col-12 p-0'>                   
-                    <Tools.renderBlock content={lastBlock} />
+    if(content.length % 2 === 0) {
+        return (
+            <div className={classes.row}>          
+            {content.map((child, i) => {             
+            return (
+                    <div className={classes.col}>                   
+                        {renderBlock({ content: child })}
+                    </div>
+                    )
+            })}                
+            </div>
+            )    
+    } else {   
+        return (
+            <div className={classes.row}>
+                {content.map((child) => { 
+                    content.pop()            
+                    return (
+                        <div className={classes.col}> 
+                            {renderBlock({ content: child })}
+                        </div>
+                    )
+                })}
+                <div className='col-12 p-0'>                   
+                    {renderBlock({ content: lastBlock })}
                 </div>                
-        </div>
-        )  
-}
+            </div>
+            )  
+    }
 }
      
 
-export default Row;
+export default Row
