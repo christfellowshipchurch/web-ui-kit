41ff55516633f4558ff66823e7d24c0c9043f185
Caleb Panza <caleb.panza@icloud.org>
2019-08-19 13:11:13 -0400

    Updated Carousel

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import { Block, Row, TextInput, Dropdown, TextArea, Carousel, Accordion, Button,
 const App = () => {
   return (
     <div className='container bg-light w-75'>
-      <Row>
-        <Media
-          layout={'default'}
-          imageUrl={'https://picsum.photos/id/652/200/300'}
-          imageAlt={'Image Alt'} />
-
-        <Media
-          layout={'default'}
-          imageUrl={'https://picsum.photos/id/652/200/300'}
-          imageAlt={'Image Alt'}
-          circle ratio='4by3' />
-      </Row>
-
-      <Row>
+      <Carousel>
         <Block
           layout={'default'}
           imageUrl={'https://picsum.photos/id/652/200/300'}
@@ -31,18 +18,15 @@ const App = () => {
           </Block.Subtitle>
 
           <Block.Title className={`titleWeight`}>
-            Default
+            One
           </Block.Title>
 
           <Block.Body className={`pt-1 pb-4 font-weight-light`}>
             {[0, 1, 2, 3, 4, 5, 6].map(n => <p>{n}</p>)}
           </Block.Body>
         </Block>
-      </Row>
-
-      <Row>
         <Block
-          layout={'inverted'}
+          layout={'default'}
           imageUrl={'https://picsum.photos/id/652/200/300'}
           imageAlt={'Image Alt'}
           ratio={'4by3'}
@@ -53,18 +37,15 @@ const App = () => {
           </Block.Subtitle>
 
           <Block.Title className={`titleWeight`}>
-            Inverted
+            Two
           </Block.Title>
 
           <Block.Body className={`pt-1 pb-4 font-weight-light`}>
             {[0, 1, 2, 3, 4, 5, 6].map(n => <p>{n}</p>)}
           </Block.Body>
         </Block>
-      </Row>
-
-      <Row>
         <Block
-          layout={'left'}
+          layout={'default'}
           imageUrl={'https://picsum.photos/id/652/200/300'}
           imageAlt={'Image Alt'}
           ratio={'4by3'}
@@ -75,36 +56,15 @@ const App = () => {
           </Block.Subtitle>
 
           <Block.Title className={`titleWeight`}>
-            Content Left
+            Three
           </Block.Title>
 
           <Block.Body className={`pt-1 pb-4 font-weight-light`}>
             {[0, 1, 2, 3, 4, 5, 6].map(n => <p>{n}</p>)}
           </Block.Body>
         </Block>
-      </Row>
+      </Carousel>
 
-      <Row>
-        <Block
-          layout={'right'}
-          imageUrl={'https://picsum.photos/id/652/200/300'}
-          imageAlt={'Image Alt'}
-          ratio={'4by3'}
-        >
-
-          <Block.Subtitle className={`text-muted font-weight-bold`}>
-            This is the subtitle
-          </Block.Subtitle>
-
-          <Block.Title className={`titleWeight`}>
-            Content Right
-          </Block.Title>
-
-          <Block.Body className={`pt-1 pb-4 font-weight-light`}>
-            {[0, 1, 2, 3, 4, 5, 6].map(n => <p>{n}</p>)}
-          </Block.Body>
-        </Block>
-      </Row>
 
     </div>
   )
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,52 +1,47 @@
 import React, { useState, useEffect } from 'react'
 import classnames from 'classnames'
-import { uniqueId } from 'lodash'
 
-
-const Carousel = ({ children }) => {
-    const id = uniqueId('carousel-')
+const Carousel = ({ children, interval }) => {
     const [index, setIndex] = useState(0)
-    const length = children.length
 
-    //Changes index in a specified interval
+    // Changes index in a specified interval
     useEffect(() => {
         const add = (index) => {
-            if (index < length - 1) {
+            if (index < children.length - 1) {
                 index += 1
             } else {
                 index = 0
             }
             return index
         }
-        const interval = setInterval(() => {
+
+        const timer = setInterval(() => {
             setIndex(add(index));
-        }, 3000);
-        return () => clearInterval(interval);
-    }, [index, length]);
+        }, interval)
 
-
+        return () => clearInterval(timer)
+    }, [index, children.length, interval])
 
     return (
-        <div id={id} className="carousel slide" data-ride="carousel">
+        <div className="carousel slide" data-ride="carousel">
             {/* <!-- Carousel indicators --> */}
-            {length &&
-                <ol class="carousel-indicators">
-                    {children.map((child, i) => {
+            {children.length &&
+                <ol className="carousel-indicators">
+                    {children.map((n, i) => {
                         const indicatorClasses = classnames(
                             i === index ? 'active' : ''
                         )
                         return (
-                            <li data-target={`#${id}`} data-slide-to="0" className={indicatorClasses} onClick={() => setIndex(i)}></li>
+                            <li key={i} className={indicatorClasses} onClick={() => setIndex(i)}></li>
                         )
                     })}
                 </ol>
-
             }
 
             {/* <!-- Wrapper for carousel items --> */}
 
-            {length &&
-                <div className="carousel-inner container d-flex align-items-center">
+            {children.length &&
+                <div className="carousel-inner container d-flex align-items-center pb-5">
                     {children.map((child, i) => {
                         const itemClasses = classnames(
                             'item',
@@ -54,7 +49,7 @@ const Carousel = ({ children }) => {
                             i === index ? 'active' : ''
                         )
                         return (
-                            <div class={itemClasses}>
+                            <div className={itemClasses} key={i}>
                                 {child}
                             </div>
                         )
@@ -63,6 +58,10 @@ const Carousel = ({ children }) => {
             }
         </div>
     )
+}
+
+Carousel.defaultProps = {
+    interval: 3000
 }
 
 export default Carousel
